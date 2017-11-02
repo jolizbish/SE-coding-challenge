19b9826ecb0973de9261324a9e825e8f18bbe178
Jordan Bishop <bishopj1292@gmail.com>
2017-11-01 20:55:09 -0500

    Implement JobDescription component skeleton

---
+++ client/JobDescription.jsx
@@ -0,0 +1,7 @@
+var JobDescription = (props) => (
+  <div className="search-bar form-inline">
+    <input className="form-control" type="textarea" />
+  </div>
+);
+
+window.JobDescription = JobDescription;
