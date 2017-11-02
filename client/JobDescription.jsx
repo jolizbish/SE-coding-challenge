076c1931e252bc865341bc94ef56364a849a72e5
Jordan Bishop <bishopj1292@gmail.com>
2017-11-01 21:15:33 -0500

    Set up rough code to POST to API on text change

--- client/JobDescription.jsx
+++ client/JobDescription.jsx
@@ -1,6 +1,6 @@
 var JobDescription = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="textarea" />
+    <input className="form-control" type="textarea" onChange={props.onTextEntryHandleChange}/>
   </div>
 );
 
--- client/app.jsx
+++ client/app.jsx
@@ -2,11 +2,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: ''
     };
+    this.onTextEntryHandleChange = this.onTextEntryHandleChange.bind(this);
   }
+
+
+
   componentDidMount() {
     console.log(this.props)
 
+  }
+
+  onTextEntryHandleChange(text) {
+    this.setState({
+      text: text.target.value
+    })
+    console.log(this.state.text, "text: ");
   }
 
   render() {
