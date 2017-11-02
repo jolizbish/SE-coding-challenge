80ce81a1fd0bf8edf349bbbf0f95d2a000545159
Jordan Bishop <bishopj1292@gmail.com>
2017-11-01 20:51:18 -0500

    Implement ItemEntry component skeleton

---
+++ client/ItemEntry.jsx
@@ -0,0 +1,13 @@
+
+var ItemEntry = (props) => (
+  <div className="item-entry" }>
+
+  </div>
+);
+
+
+ItemEntry.propTypes = {
+  item: React.PropTypes.object.isRequired
+};
+
+window.ItemEntry = ItemEntry;
