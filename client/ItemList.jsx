7d09941aac7c6069e8a9bb3460bc1c41450deb8f
Jordan Bishop <bishopj1292@gmail.com>
2017-11-01 20:48:27 -0500

    Implement ItemList component skeleton

---
+++ client/ItemList.jsx
@@ -0,0 +1,13 @@
+var ItemList = (props) => (
+  <div className="item-list">
+
+  </div>
+);
+
+
+ItemList.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
+
+
+window.ItemList = ItemList;
